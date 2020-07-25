f41efc4f4c5c31f309ee61d9f6ab9f1fba811247
Vishal_Bait <vishalbait01@gmail.com>
2020-07-25 21:19:02 +0530

    Shift edit and update operation to ajax node-express-mysql ocrud operation

--- Node-Express-Mysql/routes/index.js
+++ Node-Express-Mysql/routes/index.js
@@ -14,9 +14,13 @@ conn.connect(function (err) {
   console.log("Database Connected");
 });
 
+//---------------------->> RENDER HOME PAGE
+
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Employee Management", success: "" });
 });
+
+//---------------------->> READ OPERATION
 
 router.post("/read", function (req, res, next) {
   let sql = "SELECT * FROM employee_information";
@@ -25,6 +29,8 @@ router.post("/read", function (req, res, next) {
     res.send({ records: data });
   });
 });
+
+//---------------------->> CREATE OPERATION
 
 router.post("/create", function (req, res, next) {
   let name = req.body.name;
@@ -46,6 +52,8 @@ router.post("/create", function (req, res, next) {
   });
 });
 
+//---------------------->> DELETE OPERATION
+
 router.post("/delete", function (req, res, next) {
   let id = req.body.deleteId;
 
@@ -56,51 +64,41 @@ router.post("/delete", function (req, res, next) {
   });
 });
 
-//---------------------->> EDIT
+//---------------------->> EDIT OPERATION
 
-router.get("/edit/:id", function (req, res, next) {
-  let id = parseInt(req.params.id);
+router.post("/edit", function (req, res, next) {
+  let id = req.body.editId;
 
   let sql = "SELECT * FROM employee_information WHERE id = ?";
 
   conn.query(sql, [id], function (err, data) {
     if (err) throw err;
-
-    res.render("edit", {
-      title: "Edit Employee Information",
-      editData: data,
-      success: "",
-    });
+    res.send({ employee: data });
   });
 });
 
+//---------------------->> UPDATE  OPERATION
+
 router.post("/update", function (req, res, next) {
-  let name = req.body.name;
-  let email = req.body.email;
-  let etype = req.body.etype;
-  let hourlyRate = req.body.hourlyRate;
-  let totalHour = req.body.totalHour;
-  let total = parseInt(req.body.totalHour) * parseInt(req.body.hourlyRate);
+  let name = req.body.updateName;
+  let email = req.body.updateEmail;
+  let etype = req.body.updateEtype;
+  let hourlyRate = req.body.updateHourlyRate;
+  let totalHour = req.body.updateTotalHour;
+  let total = parseInt(totalHour) * parseInt(hourlyRate);
+  let id = req.body.hiddenId;
 
   let sql =
-    "UPDATE employee_information SET name = ?, email = ?, etype = ?, hourlyRate = ?, totalHour = ?, total = ?";
+    "UPDATE employee_information SET name = ?, email = ?, etype = ?, hourlyRate = ?, totalHour = ?, total = ? WHERE id = ?";
 
-  conn.query(sql, [name, email, etype, hourlyRate, totalHour, total], function (
-    err,
-    data
-  ) {
-    if (err) throw err;
-    // Find Query
-    let sql = "SELECT * FROM employee_information";
-    conn.query(sql, function (err, data) {
+  conn.query(
+    sql,
+    [name, email, etype, hourlyRate, totalHour, total, id],
+    function (err, data) {
       if (err) throw err;
-      res.render("index", {
-        title: "Employee Management System",
-        success: "Data Updated Succesfully",
-        records: data,
-      });
-    });
-  });
+      res.send("Data Updated Successfully");
+    }
+  );
 });
 
 module.exports = router;
